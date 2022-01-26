ce909c4cf3a42851e2def337fb7aebb2f7551623
Jatin-Bhardwaj <kaijatin1@gmail.com>
2022-01-26 10:27:10 +0530

    first-commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * Jatin CLI Alerts
+ * 
+ * Cross platform CLI Alerts with colors
+ * Workds on macOS , Linux and Windows
+ * Alerts : `success` , `info`, `warning` , `error`
+ * @author Jatin Bhardwaj <>
+ */
